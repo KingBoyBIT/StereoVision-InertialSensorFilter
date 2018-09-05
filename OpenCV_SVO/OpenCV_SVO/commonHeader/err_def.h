166b1057b09b7f2ff07c27e805d0af9be2e2e15c
KingBoyBIT <405613185@qq.com>
2018-09-05 22:27:41 +0800

    debug

--- OpenCV_SVO/OpenCV_SVO/commonHeader/err_def.h
+++ OpenCV_SVO/OpenCV_SVO/commonHeader/err_def.h
@@ -2,6 +2,7 @@
 #define ERR_DEF_H
 
 #define ERR_OPEN_IMG			0X00000001
+#define ERR_OPEN_CAM			0X00000001
 
 #endif // !ERR_DEF_H
 
--- OpenCV_SVO/OpenCV_SVO/main.cpp
+++ OpenCV_SVO/OpenCV_SVO/main.cpp
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "opencv2\opencv.hpp"
+#include <opencv2\core\core.hpp>
+#include <opencv2\opencv.hpp>
+#include <opencv\highgui.h>
 #include "commonHeader\types.h"
 #include "commonHeader\err_def.h"
 #include "commonHeader\alldef.h"
 
 using namespace cv;
-
+UINT32 ret = 0;
 int main()
 {
-	UINT32 ret = 0;
-	Mat img = imread("Lenna_(test_image).png");
-	if (img.data == NULL)
-	{
-		return ERR_OPEN_IMG;
-	}
-#if DEBUG_FLAG
-	printf("size of img:%d,%d\n", img.rows, img.cols);
-#endif // DEBUG_FLAG
+	
+	VideoCapture cap;
+	cap.open(0);
 
-	imshow("lena", img);
-	waitKey(1000);
-	return 0;
+	if (cap.isOpened() != true)
+	{
+		return ERR_OPEN_CAM;
+	}
+	while (1)
+	{
+		//Mat img = imread("Lenna_(test_image).png");
+		Mat frame;
+		cap >> frame;
+		if (frame.data == NULL)
+		{
+			return ERR_OPEN_IMG;
+		}
+#if DEBUG_FLAG
+		printf("size of img:%d,%d\n", frame.rows, frame.cols);
+		imshow("lena", frame);
+#if 1
+		ret = waitKey(20);//会崩溃，没事不要用
+#endif
+		if (ret != -1)
+		{
+			return ret;
+		}
+#endif // DEBUG_FLAG
+	}
+	return ret;
 }

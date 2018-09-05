adcec699aebef47a3930d2d72c230fc3ffa709d7
KingBoyBIT <405613185@qq.com>
2018-09-05 21:47:29 +0800

    debug

---
+++ OpenCV_SVO/OpenCV_SVO/commonHeader/alldef.h
@@ -0,0 +1,6 @@
+#ifndef ALLDEF_H
+#define ALLDEF_H
+
+#define DEBUG_FLAG				1
+
+#endif // !ALLDEF_H
--- OpenCV_SVO/OpenCV_SVO/main.cpp
+++ OpenCV_SVO/OpenCV_SVO/main.cpp
@@ -2,7 +2,7 @@
 #include "opencv2\opencv.hpp"
 #include "commonHeader\types.h"
 #include "commonHeader\err_def.h"
-
+#include "commonHeader\alldef.h"
 using namespace cv;
 
 int main()
@@ -13,6 +13,10 @@ int main()
 	{
 		return ERR_OPEN_IMG;
 	}
+#if DEBUG_FLAG
+	printf("size of img:%f,%f\n", img.rows, img.cols);
+#endif // DEBUG_FLAG
+
 	imshow("lena", img);
 	waitKey(1000);
 	return 0;

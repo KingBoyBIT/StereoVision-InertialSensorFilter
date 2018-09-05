9fbffeb53b8fdce87ad56c23ec32b989e7f5cc76
KingBoyBIT <405613185@qq.com>
2018-09-05 22:03:13 +0800

    编译

--- OpenCV_SVO/OpenCV_SVO/main.cpp
+++ OpenCV_SVO/OpenCV_SVO/main.cpp
@@ -1,20 +1,22 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "opencv2\opencv.hpp"
 #include "commonHeader\types.h"
 #include "commonHeader\err_def.h"
 #include "commonHeader\alldef.h"
+
 using namespace cv;
 
 int main()
 {
 	UINT32 ret = 0;
-	Mat img = imread("test.jpg");
+	Mat img = imread("Lenna_(test_image).png");
 	if (img.data == NULL)
 	{
 		return ERR_OPEN_IMG;
 	}
 #if DEBUG_FLAG
-	printf("size of img:%f,%f\n", img.rows, img.cols);
+	printf("size of img:%d,%d\n", img.rows, img.cols);
 #endif // DEBUG_FLAG
 
 	imshow("lena", img);

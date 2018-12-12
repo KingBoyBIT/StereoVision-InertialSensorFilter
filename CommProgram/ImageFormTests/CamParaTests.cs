fea3bd09b9f583a7fd94141ceaa923cf29b45189
KingBoyBIT <405613185@qq.com>
2018-12-12 23:50:12 +0800

    unittest

--- CommProgram/ImageFormTests/CamParaTests.cs
+++ CommProgram/ImageFormTests/CamParaTests.cs
@@ -14,9 +14,9 @@ namespace ImageForm.Tests
 		///</summary>
 		private TestContext testContextInstance;
 
-		const string datasetFilePath = @"D:\Codes\githubcodes\StereoVision-InertialSensorFilter\datasetanalysis\dataset";
-		const string testPath = @"\testing";
-		const string trainPath = @"\training";
+		const string datasetFilePath = @"D:\Codes\githubcodes\StereoVision-InertialSensorFilter\datasetanalysis\dataset\data_scene_flow\";
+		const string testPath = @"testing\";
+		const string trainPath = @"training\";
 		string[] trainPathList = new string[11];
 
 		#region 附加测试特性
@@ -39,17 +39,17 @@ namespace ImageForm.Tests
 		[TestInitialize()]
 		public void MyTestInitialize()
 		{
-			trainPathList[0] = "disp_noc_0";             //左灰度相机视差图，无遮挡
-			trainPathList[1] = "disp_noc_1";             //右灰度相机视差图，无遮挡
-			trainPathList[2] = "disp_occ_0";             //左灰度相机视差图，有遮挡
-			trainPathList[3] = "disp_occ_1";             //右灰度相机视差图，有遮挡
-			trainPathList[4] = "flow_noc";               //表示在求取光流的两幅图像上都出现的区域的位移误差
-			trainPathList[5] = "flow_occ";               //表示在第一幅图像中可见而在第二副图像中被遮挡的区域的位移误
-			trainPathList[6] = "image_2";                    //左侧彩色相机
-			trainPathList[7] = "image_3";                    //右侧彩色相机
-			trainPathList[8] = "obj_map";                    //目标
-			trainPathList[9] = "viz_flow_occ";           //光流可视化，有遮挡
-			trainPathList[10] = "viz_flow_occ_dilate_1";    //光流可视化，有遮挡，膨胀
+			trainPathList[0] = @"disp_noc_0\";             //左灰度相机视差图，无遮挡
+			trainPathList[1] = @"disp_noc_1\";             //右灰度相机视差图，无遮挡
+			trainPathList[2] = @"disp_occ_0\";             //左灰度相机视差图，有遮挡
+			trainPathList[3] = @"disp_occ_1\";             //右灰度相机视差图，有遮挡
+			trainPathList[4] = @"flow_noc\";               //表示在求取光流的两幅图像上都出现的区域的位移误差
+			trainPathList[5] = @"flow_occ\";               //表示在第一幅图像中可见而在第二副图像中被遮挡的区域的位移误
+			trainPathList[6] = @"image_2\";                    //左侧彩色相机
+			trainPathList[7] = @"image_3\";                    //右侧彩色相机
+			trainPathList[8] = @"obj_map\";                    //目标
+			trainPathList[9] = @"viz_flow_occ\";           //光流可视化，有遮挡
+			trainPathList[10] = @"viz_flow_occ_dilate_1\";    //光流可视化，有遮挡，膨胀
 		}
 		//
 		//使用 TestCleanup 在运行完每个测试后运行代码
@@ -72,7 +72,7 @@ namespace ImageForm.Tests
 			Image<Bgr, byte> image = new Image<Bgr, byte>(path);
 
 			CvInvoke.Imshow("testimage", image);
-			//System.Console.WriteLine(datasetFilePath + trainPath + trainPathList[0]);
+			System.Console.WriteLine(datasetFilePath + trainPath + trainPathList[0]);
 
 		}
 	}

c3e5c8172a456ff80ae83548b50231c965774d3e
KingBoyBIT <405613185@qq.com>
2018-12-14 00:00:46 +0800

    获取深度数据

--- CommProgram/ImageForm/ImageForm.Designer.cs
+++ CommProgram/ImageForm/ImageForm.Designer.cs
@@ -29,6 +29,8 @@
 		private void InitializeComponent()
 		{
 			this.LoadImageBtn = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// LoadImageBtn
@@ -41,15 +43,26 @@
 			this.LoadImageBtn.UseVisualStyleBackColor = true;
 			this.LoadImageBtn.Click += new System.EventHandler(this.LoadImageBtn_Click);
 			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(13, 42);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1205, 460);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
 			// ImageForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.ClientSize = new System.Drawing.Size(1230, 514);
+			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.LoadImageBtn);
 			this.Name = "ImageForm";
 			this.Text = "ImageForm";
 			this.Load += new System.EventHandler(this.ImageForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -57,6 +70,7 @@
 		#endregion
 
 		private System.Windows.Forms.Button LoadImageBtn;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
--- CommProgram/ImageForm/ImageForm.cs
+++ CommProgram/ImageForm/ImageForm.cs
@@ -1,4 +1,5 @@
 ﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,9 @@ namespace ImageForm
 {
 	public partial class ImageForm : Form
 	{
-		const string datasetFilePath = @"D:\Codes\githubcodes\StereoVision-InertialSensorFilter\datasetanalysis\dataset";
-		const string testPath = @"\testing";
-		const string trainPath = @"\training";
+		const string datasetFilePath = @"D:\Codes\githubcodes\StereoVision-InertialSensorFilter\datasetanalysis\dataset\data_scene_flow\";
+		const string testPath = @"\testing\";
+		const string trainPath = @"\training\";
 		string[] trainPathList = new string[11];
 
 		
@@ -28,18 +29,18 @@ namespace ImageForm
 
 		private void ImageForm_Load(object sender, EventArgs e)
 		{
-			trainPathList[0] = "disp_noc_0";             //左灰度相机视差图，无遮挡
-			trainPathList[1] = "disp_noc_1";             //右灰度相机视差图，无遮挡
-			trainPathList[2] = "disp_occ_0";             //左灰度相机视差图，有遮挡
-			trainPathList[3] = "disp_occ_1";             //右灰度相机视差图，有遮挡
-			trainPathList[4] = "flow_noc";               //表示在求取光流的两幅图像上都出现的区域的位移误差
-			trainPathList[5] = "flow_occ";               //表示在第一幅图像中可见而在第二副图像中被遮挡的区域的位移误
-			trainPathList[6] = "image_2";                    //左侧彩色相机
-			trainPathList[7] = "image_3";                    //右侧彩色相机
-			trainPathList[8] = "obj_map";                    //目标
-			trainPathList[9] = "viz_flow_occ";           //光流可视化，有遮挡
-			trainPathList[10] = "viz_flow_occ_dilate_1";    //光流可视化，有遮挡，膨胀
-			
+			trainPathList[0] = @"disp_noc_0\";             //左灰度相机视差图，无遮挡
+			trainPathList[1] = @"disp_noc_1\";             //右灰度相机视差图，无遮挡
+			trainPathList[2] = @"disp_occ_0\";             //左灰度相机视差图，有遮挡
+			trainPathList[3] = @"disp_occ_1\";             //右灰度相机视差图，有遮挡
+			trainPathList[4] = @"flow_noc\";               //表示在求取光流的两幅图像上都出现的区域的位移误差
+			trainPathList[5] = @"flow_occ\";               //表示在第一幅图像中可见而在第二副图像中被遮挡的区域的位移误
+			trainPathList[6] = @"image_2\";                    //左侧彩色相机
+			trainPathList[7] = @"image_3\";                    //右侧彩色相机
+			trainPathList[8] = @"obj_map\";                    //目标
+			trainPathList[9] = @"viz_flow_occ\";           //光流可视化，有遮挡
+			trainPathList[10] = @"viz_flow_occ_dilate_1\";    //光流可视化，有遮挡，膨胀
+
 		}
 
 		/// <summary>
@@ -49,8 +50,34 @@ namespace ImageForm
 		/// <param name="e"></param>
 		private void LoadImageBtn_Click(object sender, EventArgs e)
 		{
-			Image<Bgr, Byte> image = new Image<Bgr, byte>("src.png");
-			CvInvoke.Imshow("test", image);
+			string imagename = "000037_10.png";
+			string path = "";
+			path = datasetFilePath + trainPath + trainPathList[0] + imagename;
+			Image<Gray, byte> image_disp_noc_0 = new Image<Gray, byte>(path);
+
+			path = datasetFilePath + trainPath + trainPathList[1] + imagename;
+			Image<Gray, byte> image_disp_noc_1 = new Image<Gray, byte>(path);
+
+			int rowNumber = image_disp_noc_0.Rows;
+			int colNumber = image_disp_noc_0.Cols;
+
+			pictureBox1.Image = image_disp_noc_0.ToBitmap();
+
+			double[,] disp_noc_0_data = new double[rowNumber, colNumber];
+			for (int i = 0; i < rowNumber; i++)
+			{
+				for (int j = 0; j < colNumber; j++)
+				{
+					//最后一个参数为通道数，例如Bgr图片的 0：蓝色，1：绿色，2：红色，Gray的0：灰度，返回TDepth类型
+					disp_noc_0_data[i, j] = image_disp_noc_0[i, j].Intensity;
+				}
+			}
+
+			Bitmap bm = image_disp_noc_0.ToBitmap();
+
+			//CvInvoke.Imshow("test", image_disp_noc_0);
+			
+			
 		}
 	}
 }

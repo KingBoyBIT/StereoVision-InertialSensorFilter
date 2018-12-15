e64ae484ec099c0238e1feaf1482f3c00dff07df
KingBoyBIT <405613185@qq.com>
2018-12-16 02:45:10 +0800

    修改

--- SLAMresearch/Environment/SelectForm.Designer.cs
+++ SLAMresearch/Environment/SelectForm.Designer.cs
@@ -54,6 +54,7 @@
 			// 
 			// checkedListBox1
 			// 
+			this.checkedListBox1.CheckOnClick = true;
 			this.checkedListBox1.FormattingEnabled = true;
 			this.checkedListBox1.Location = new System.Drawing.Point(15, 47);
 			this.checkedListBox1.Name = "checkedListBox1";
